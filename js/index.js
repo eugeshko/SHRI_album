d0ee57c2845e6785bc056d2b67cd565a89036128
Evgeniya Kostenko <erunda_88@mail.ru>
2013-10-13 00:49:01 +0400

    add backbone view
    add backbone view for students with rendering method

--- js/index.js
+++ js/index.js
@@ -1,41 +1,37 @@
-var student_model = Backbone.Model.extend({
-
-  paint: function() {
-
-    var source = $("#students_entry_template").html();
-    var template = Handlebars.compile(source);
-    var html = template(this.attributes);
-    $('#students').append(html);
-
-  }
-
-});
-
-var students_collection = Backbone.Collection.extend({
-
-  model: student_model,
-  paint: function(){
-
-    this.each(function(student) {
-
-      student.paint();
-
-    });
-
-  }
-
-});
-
-var students = new students_collection;
-
-$.getJSON('/js/students_JSON.json', function(data){
-
-    for (var i = 0; i < data.length; i++) {
-
-      var student = new student_model(data[i]);
-      students.push(student);
-
+var student_model_type = Backbone.Model.extend({
+    paint: function() {
+        var source = $("#students_entry_template").html();
+        var template = Handlebars.compile(source);
+        var html = template(this.attributes);
+        $("#students").append(html);
     }
-    students.paint();
-    
 });
+
+var students_collection_type = Backbone.Collection.extend({
+    model: student_model_type,
+    paint: function(){
+        this.each(function(student) {
+            student.paint();
+        });
+    }
+});
+
+var student_view_type = Backbone.View.extend({
+    tagName: "div",
+    className: "student",
+    render: function() {
+        $.getJSON("/js/students_JSON.json", function(data){
+            _.map(data, function(student){ 
+                var student = new student_model_type(student);
+                students.push(student);
+            });
+        students.paint();
+        });
+    }
+});
+
+var students = new students_collection_type;
+
+var students_view = new student_view_type;
+
+students_view.render();
