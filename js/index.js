b58ac89c5f1fc7f2f0561a7abebe8fd4da7416d1
Evgeniya Kostenko <erunda_88@mail.ru>
2013-10-13 10:52:42 +0400

    improve model, view, controller for students
    improve model, view, controller for students

--- js/index.js
+++ js/index.js
@@ -1,55 +1,58 @@
-var student_model_type = Backbone.Model.extend({
-    paint: function() {
-        var source = $("#students_entry_template").html();
-        var template = Handlebars.compile(source);
-        var html = template(this.attributes);
-        $("#students").append(html);
-    }
-});
+var student_model_type = Backbone.Model.extend({});
 
 var students_collection_type = Backbone.Collection.extend({
     model: student_model_type,
-    paint: function(){
-        this.each(function(student) {
-            student.paint();
-        });
-    },
-    open_about: function(id) {
-        $(".student > .about_student").hide(100);
-        $("#" + id + ".student").prependTo($("#students"));
-        $("#" + id + " > .about_student").show(1000);
-    }
 });
+
+var students = new students_collection_type();
 
 var student_view_type = Backbone.View.extend({
     tagName: "div",
     className: "student",
     render: function() {
-        $.getJSON("/js/students_JSON.json", function(data){
-            _.map(data, function(student){ 
-                var student = new student_model_type(student);
-                students.push(student);
+        var source = $("#students_entry_template").html();
+        var template = Handlebars.compile(source);
+        if (this.model !== undefined){
+            this.model.each(function(student) {
+                var html = template(student.attributes);
+                $("#students").append(html);    
             });
-        students.paint();
-        });
+        };
+        return this;
+    },
+    initialize: function() {
+        this.listenTo(students, "change", this.render);
+    },
+    open_about: function(id) {
+        $(".student > .about_student").hide(100);
+        $("#" + id + ".student").prependTo($("#students"));
+        $("#" + id + " > .about_student").show(1000);
+        $('html, body').animate({
+        scrollTop: $("#students").offset().top
+        }, 1000);
     }
 });
+
+var students_view = new student_view_type({model: students});
+
+$.getJSON("/js/students_JSON.json", function(data){
+    _.map(data, function(student_data){ 
+        var student = new student_model_type(student_data);
+        students.push(student);
+    });
+    students_view.render();
+});
+
 
 var students_workspace = Backbone.Router.extend({
     routes: {
         "!/students/:id": "open_about"
     },
     open_about: function(id) {
-        students.open_about(id);
+        students_view.open_about(id);
     }
 });
-
-var students = new students_collection_type;
-
-var students_view = new student_view_type;
 
 var controller = new students_workspace();
 
 Backbone.history.start();
-students_view.render();
-
